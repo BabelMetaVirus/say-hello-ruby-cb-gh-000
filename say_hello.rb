b969c93dbe912bce869f4219d8db0b1f46a7e93a
Ben Francisco <bpfranci@math.tamu.edu>
2019-08-18 19:51:36 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -2,5 +2,5 @@
 # say_hello:  Takes in the name of a person and prints out a greeting.
 #             Uses a standard argument of "Ruby Programmer"
 def say_hello(name = "Ruby Programmer")
-  puts "Hello #{name}"
+  puts "Hello #{name}!"
 end
